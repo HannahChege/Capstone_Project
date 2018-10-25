06071c3dcd806e40f1141e1ecd71b0d31fd9f0dd
HannahChege <chegehannah45@gmail.com>
2018-10-25 20:26:10 +0300

    Changes made

--- foody/admin.py
+++ foody/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import food,Restratuant
+from django.contrib.gis.admin import GeoModelAdmin
+from .models import food,Restaurant
 # Register your models here.
 admin.site.register(food)
-admin.site.register(Restratuant)
+admin.site.register(Restaurant)
+ 
